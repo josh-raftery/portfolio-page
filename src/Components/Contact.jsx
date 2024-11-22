57c0953d794f3291aef304975cc6f4141b04226a
= <joshuaraftery99@gmail.com>
2024-11-22 16:55:52 +0000

    added email functionality - debugging

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,17 +1,67 @@
+import { useState } from "react";
 import NextPage from "./NextPage";
+import emailjs from "emailjs-com";
 
 export default function Contact() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .send(
+        "service_r59yvne", // Replace with your EmailJS service ID
+        "template_5jnob7b", // Replace with your EmailJS template ID
+        formData
+      )
+      .then(
+        (result) => {
+          alert("Message Sent Successfully!");
+        },
+        (error) => {
+          console.error(error.text, '<----');
+        }
+      );
+  };
+
   return (
     <div className="page-height max-w-[500px] m-auto p-4">
       <h2 className="text-5xl mt-20 mb-10 font-black">Contact.</h2>
       <p>
         Get in touch or email me directly on <b>joshuaraftery99@gmail.com</b>
       </p>
-      <form className="grid gap-10 mt-10">
-        <input placeholder="Name" className="light-border"  ></input>
-        <input placeholder="Email" className="light-border" ></input>
-        <textarea placeholder="Message" className="light-border" rows="8" ></textarea>
-        <button className="light-border">Send Message</button>
+      <form onSubmit={sendEmail} className="grid gap-10 mt-10">
+        <input
+          placeholder="Name"
+          className="light-border"
+          value={formData.name}
+          name="name"
+          onChange={handleChange}
+        ></input>
+        <input
+          placeholder="Email"
+          className="light-border"
+          value={formData.email}
+          name="email"
+          onChange={handleChange}
+        ></input>
+        <textarea
+          placeholder="Message"
+          className="light-border"
+          value={formData.message}
+          name="message"
+          rows="8"
+          onChange={handleChange}
+        ></textarea>
+        <button type="submit" className="light-border">Send Message</button>
       </form>
       <NextPage text={"Back to Home"} />
     </div>
--- src/Components/NextPage.jsx
+++ src/Components/NextPage.jsx
@@ -2,10 +2,9 @@ import Next from "./Next";
 
 export default function NextPage({text}){
     return(
-        // <div className="flex justify-center gap-2 m-10 cursor-pointer" >
-        //     <p>{text}</p>
-        //     <Next/>
-        // </div>
-        <></>
+        <div className="flex justify-center gap-2 m-10 cursor-pointer" >
+            <p>{text}</p>
+            <Next/>
+        </div>
     )
 }
